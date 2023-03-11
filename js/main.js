220145b953c1f8f8844ba4afa5851b31c3a1507b
Vladimir Verbitskiy <vova10barcelona@mail.ru>
2023-03-11 03:12:45 +0300

    Добавляет генерацию значений объекта, кроме id и не повторения значений

--- js/main.js
+++ js/main.js
@@ -14,11 +14,50 @@
   Случайное число от 15 до 200.
 */
 
-const getRandomInteger = (minNumber, maxNumber) => {
-  return Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
-};
+const DESCRIPTION = [
+  'Описание 1',
+  'Описание 2',
+  'Описание 3',
+  'Описание 4',
+  'Описание 5',
+  'Описание 6',
+  'Описание 7',
+  'Описание 8',
+  'Описание 9',
+  'Описание 10',
+  'Описание 11',
+  'Описание 12',
+  'Описание 13',
+];
 
-// console.log(getRandomInteger(3, 6));
+const MESSAGE = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const NAME = [
+  'Вова',
+  'Стёпа',
+  'Лиза',
+  'Марина',
+  'Саша',
+  'Артём',
+  'Лёша',
+  'Влад'
+];
+
+const PHOTO_CARDS_COUNT = 25;
+
+// Выбор случайного числа из диапазоне
+const getRandomInteger = (minNumber, maxNumber) => Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+
+// Выбор случайного элемента массива
+const getRandomArrayElement = (array) => array[getRandomInteger(0, array.length - 1)];
+
 
 const createPhotoDescription = () => {
   const randomInteger = getRandomInteger(1, 25);
@@ -26,14 +65,18 @@ const createPhotoDescription = () => {
   return {
     id: randomInteger,
     url: `photos/${randomInteger}`,
-    // description: '',
-    // likes:
+    description: getRandomArrayElement(DESCRIPTION),
+    likes: getRandomInteger(15, 200),
+    comments: {
+      id: getRandomInteger(1, 1000),
+      avatar: `img/avatar-${getRandomInteger(1, 6)}`,
+      message: getRandomArrayElement(MESSAGE),
+      name: getRandomArrayElement(NAME),
+    }
   };
 };
 
-// console.log(createPhotoDescription());
-
-const createPhotoInfo = Array.from({ length: 25 }, createPhotoDescription);
+const createPhotoInfo = Array.from({ length: PHOTO_CARDS_COUNT }, createPhotoDescription);
 
 console.log(createPhotoInfo);
 
