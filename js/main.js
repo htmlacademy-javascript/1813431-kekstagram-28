da721ed6f8c892a3cf80d044017c3969703cf751
Vladimir Verbitskiy <vova10barcelona@mail.ru>
2023-03-16 00:23:35 +0300

    Добавляет функцию для возврата уникальных целых чисел

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@
   Случайное число от 15 до 200.
 */
 
-const DESCRIPTION = [
+const DESCRIPTIONS = [
   'Описание 1',
   'Описание 2',
   'Описание 3',
@@ -28,9 +28,9 @@ const DESCRIPTION = [
   'Описание 11',
   'Описание 12',
   'Описание 13',
+  'Описание 14',
 ];
-
-const MESSAGE = [
+const MESSAGES = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -38,8 +38,7 @@ const MESSAGE = [
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
-
-const NAME = [
+const NAMES = [
   'Вова',
   'Стёпа',
   'Лиза',
@@ -51,38 +50,79 @@ const NAME = [
 ];
 
 const PHOTO_CARDS_COUNT = 25;
+const AVATAR_COUNT = 6;
+const MIN_LIKE_COUNT = 15;
+const MAX_LIKE_COUNT = 200;
+const COMMENT_COUNT = 5;
 
 // Выбор случайного числа из диапазоне
-const getRandomInteger = (minNumber, maxNumber) => Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+const getRandomInteger = (minNumber, maxNumber) =>
+  Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
 
-// Выбор случайного элемента массива
-const getRandomArrayElement = (array) => array[getRandomInteger(0, array.length - 1)];
+// Неповторяющиеся числа
+const getUniqueValue = (min, max) => {
+  const previousValue = [];
 
-
-const createPhotoDescription = () => {
-  const randomInteger = getRandomInteger(1, 25);
-
-  return {
-    id: randomInteger,
-    url: `photos/${randomInteger}`,
-    description: getRandomArrayElement(DESCRIPTION),
-    likes: getRandomInteger(15, 200),
-    comments: {
-      id: getRandomInteger(1, 1000),
-      avatar: `img/avatar-${getRandomInteger(1, 6)}`,
-      message: getRandomArrayElement(MESSAGE),
-      name: getRandomArrayElement(NAME),
+  return function () {
+    let currentValue = getRandomInteger(min, max);
+    if (previousValue.length >= (max - min + 1)) {
+      return null;
     }
+    while (previousValue.includes(currentValue)) {
+      currentValue = getRandomInteger(min, max);
+    }
+    previousValue.push(currentValue);
+    return currentValue;
   };
 };
 
+const createUniqueValue = getUniqueValue(1, 25);
+
+// Выбор случайного элемента массива
+
+const getRandomArrayElement = (array) =>
+  array[getRandomInteger(0, array.length - 1)];
+
+// Генерирует id по возрастанию, начиная с 1
+
+const getIdGenerator = () => {
+  let lastGeneratedId = 0;
+
+  return () => {
+    lastGeneratedId += 1;
+    return lastGeneratedId;
+  };
+};
+
+const generateCommentId = getIdGenerator();
+
+// Генерация комментария к фото
+
+const getComments = () => ({
+  id: generateCommentId(),
+  avatar: `img/avatar-${getRandomInteger(1, AVATAR_COUNT)}`,
+  message: getRandomArrayElement(MESSAGES),
+  name: getRandomArrayElement(NAMES),
+});
+
+// Информация о фото
+
+const createPhotoDescription = () => {
+  const uniqueValue = createUniqueValue();
+
+  return {
+    id: uniqueValue,
+    url: `photos/${uniqueValue}`,
+    description: getRandomArrayElement(DESCRIPTIONS),
+    likes: getRandomInteger(MIN_LIKE_COUNT, MAX_LIKE_COUNT),
+    comments: Array.from({ length: getRandomInteger(1, COMMENT_COUNT) }, getComments)
+  };
+};
+
+// Создание массива из объектов фото
+
 const createPhotoInfo = Array.from({ length: PHOTO_CARDS_COUNT }, createPhotoDescription);
 
-console.log(createPhotoInfo);
+const getPhotoCards = () => createPhotoInfo;
 
-// const getRandomInteger = (a, b) => {
-//   const lower = Math.ceil(Math.min(a, b));
-//   const upper = Math.floor(Math.max(a, b));
-//   const result = Math.random() * (upper - lower + 1) + lower;
-//   return Math.floor(result);
-// };
+getPhotoCards();
